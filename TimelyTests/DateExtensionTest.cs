672f311a201a2c3d4879a09d729cb01c073d9fc4
Raphael Guntersweiler <raphael@rgunti.ch>
2017-07-22 23:08:56 +0200

    Written Tests for Date Extensions

---
+++ TimelyTests/DateExtensionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimelyExtensions;
+
+namespace TimelyTests
+{
+    [TestClass]
+    public class DateExtensionTest
+    {
+        [TestMethod]
+        public void CheckMonthJanuary()
+        {
+            Assert.AreEqual(new DateTime(2015, 1, 15), 15.January(2015));
+        }
+
+        [TestMethod]
+        public void CheckShortMonthJanuary()
+        {
+            Assert.AreEqual(new DateTime(2015, 1, 15), 15.Jan(2015));
+        }
+
+        [TestMethod]
+        public void NullableYearTest()
+        {
+            Assert.AreEqual(new DateTime(DateTime.Now.Year, 10, 2), 2.October());
+        }
+    }
+}
