fb783b6efa46320755db0d5a148edef565b085bf
Raphael Guntersweiler <raphael@rgunti.ch>
2017-07-22 23:20:12 +0200

    Added Time Extensions

---
+++ TimelyExtensions/TimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TimelyExtensions
+{
+    public static class TimeExtensions
+    {
+        public static TimeSpan Milliseconds(this int ms) { return TimeSpan.FromMilliseconds(ms); }
+        public static TimeSpan Seconds(this int sec) { return TimeSpan.FromSeconds(sec); }
+        public static TimeSpan Minutes(this int min) { return TimeSpan.FromMinutes(min); }
+        public static TimeSpan Hours(this int hrs) { return TimeSpan.FromHours(hrs); }
+        public static TimeSpan Days(this int days) { return TimeSpan.FromDays(days); }
+    }
+}
